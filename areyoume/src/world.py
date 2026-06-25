4df06ec551fa4c24f8bd2df722528bf11c4bd461
Rtouch-dev <shahar.dayan.shaked@gmail.com>
2026-06-25 12:42:27 +0300

    Add logging configuration to setup_world function

--- areyoume/src/world.py
+++ areyoume/src/world.py
@@ -1,5 +1,6 @@
 import logging
 from ursina import *
 def setup_world():
+    logging.basicConfig(level=logging.DEBUG)
     Sky(texture='../assets/textures/sky.jpg')
     logging.debug("World Environment Loaded.") #why not
